0b22036004d8658da333d32a71f367877a5220f7
Aidana <justaidana99@gmail.com>
2022-06-13 07:54:35 -0400

    13.06 edit

--- server.js
+++ server.js
@@ -5,3 +5,25 @@ const app = new Express();
 app.use(Express.static('static')).listen(5000, () => {
     console.log('server listens on port 5000');
 });
+
+
+// camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+
+// function camelize(str) {
+//         let arr = str.split('');
+        
+//         for(let i=0;i<arr.length;i++){
+//           if(arr[i]=='-'){
+//             if(i < arr.length-1){
+//               arr[i+1]=arr[i+1].toUpperCase();
+//             }
+//             arr.splice(i,1);
+//           }
+//         }
+        
+//         return arr.join('');
+// }
+// console.log(camelize("background-color-image"));
---
+++ static/main.js
@@ -0,0 +1,5 @@
+const enrollBtn = document.querySelector('.enroll-btn');
+
+enrollBtn.addEventListener('click', () => {
+    alert('Enroll btn');
+});
