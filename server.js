cb0616ec6876d6ffe02b32e0fc3d6fcd598530fd
Aidana <justaidana99@gmail.com>
2022-06-28 13:11:12 -0400

    28.06 edit

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const Express = require('express');
 
 const app = new Express();
 
+app.use(Express.json());
+
+const userDB = {};
+const userResponse = {};
+
+app.post('/enroll', (req, res) => {
+    userDB.userName = req.body.userName;
+    userDB.userSurname = req.body.userSurname;
+
+    userResponse.message = `New user ${userDB.userName} is created`;
+
+    res.json(userResponse);
+});
+
 app.use(Express.static('static')).listen(5000, () => {
     console.log('server listens on port 5000');
 });
--- static/main.js
+++ static/main.js
@@ -1,5 +1,35 @@
-const enrollBtn = document.querySelector('.enroll-btn');
+const submitEnrollBtn = document.querySelector('.submit-Enroll-Btn');
+const nameInput = document.querySelector('.name-input');
+const surnameInput = document.querySelector('.surname-input');
+const birthdayInput = document.querySelector('.birthday-input');
+const emailInput = document.querySelector('.email-input');
+const workPhoneCheck = document.querySelector('.work-phone-check');
+const homePhoneCheck = document.querySelector('.home-phone-check');
+const phoneInput = document.querySelector('.phone-input');
+const onlineMode = document.querySelector('.online-mode-check');
+const offlineMode = document.querySelector('.inPerson-mode-check');
 
-enrollBtn.addEventListener('click', () => {
-    alert('Enroll btn');
-});
+
+submitEnrollBtn?.addEventListener('click', () => {
+    const userData = {
+        userName: nameInput.value,
+        userSurname: surnameInput.value,
+        userBirthday: birthdayInput.value,
+        userEmail: emailInput.value,
+        userWorkPhonecheck: workPhoneCheck.value,
+        userhomePhoneCheck: homePhoneCheck.value,
+        userphone: phoneInput.value,
+        onlineModepreference: onlineMode.value,
+        inPersonModePreference: offlineMode.value
+
+    }
+    console.log(userData);
+    fetch('/enroll', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    }).then(response => response.json())
+        .then(data => alert(data.message));
+})
